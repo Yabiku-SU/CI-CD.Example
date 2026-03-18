6b603a677f4a6495e31bba4baaff0c521ce69d4f
Nikita Sadovskij <nik.sadovskij@gmail.com>
2026-03-18 18:21:47 +0100

    Initial commit

---
+++ CI-CD.Example/CI_CD_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CD_ExampleApp.swift
+//  CI-CD.Example
+//
+//  Created by Nikita Sadouski on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CI_CD_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
